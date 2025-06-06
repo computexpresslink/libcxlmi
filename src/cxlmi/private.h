da7ef7e1128437240c7d5349b1fcd97299ea0bd4
Anisa Su <anisa.su@samsung.com>
2025-06-06 07:29:05 +0000

    Set Feature (Opcode 0502h)
    Add support for command 0502h Set Feature as per CXL r3.2 Section
    8.2.10.6.3 Set Feature
    
    Signed-off-by: Anisa Su <anisa.su@samsung.com>

--- src/cxlmi/private.h
+++ src/cxlmi/private.h
@@ -211,6 +211,7 @@ enum {
 	FEATURES	= 0x05,
 	#define GET_SUPPORTED_FEATURES 0x0
 	#define GET_FEATURE 0x1
+	#define SET_FEATURE 0x2
     IDENTIFY    = 0x40,
 	#define MEMORY_DEVICE 0x0
     CCLS        = 0x41,
