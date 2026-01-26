computexpresslink/libcxlmi

commit 2f14ae0df3f49f1c6879cbb58a2d7c37164d83e3

lib/fmapi: add get VCS info command
This was missing from the VCS command set.

Signed-off-by: Davidlohr Bueso <dave@stgolabs.net>

src/cxlmi/commands.c
@@ -2411,6 +2411,53 @@ CXLMI_EXPORT int cxlmi_cmd_fmapi_get_domain_validation_sv(struct cxlmi_endpoint 
 	return rc;
 }
 
+CXLMI_EXPORT int cxlmi_cmd_fmapi_get_vcs_info(struct cxlmi_endpoint *ep,
+			    struct cxlmi_tunnel_info *ti,
+			    struct cxlmi_cmd_fmapi_get_vcs_info_req *in,
+			    struct cxlmi_cmd_fmapi_get_vcs_info_rsp *ret)
+{
+	struct cxlmi_cmd_fmapi_get_vcs_info_req *req_pl;
+	struct cxlmi_cmd_fmapi_get_vcs_info_rsp *rsp_pl;
+	_cleanup_free_ struct cxlmi_cci_msg *req = NULL;
+	_cleanup_free_ struct cxlmi_cci_msg *rsp = NULL;
+	ssize_t req_sz, rsp_sz, rsp_pl_sz;
+	int rc = -1;
+
+	req_sz = sizeof(*req) + sizeof(*req_pl) + in->num_vcs;
+	req = calloc(1, req_sz);
+	if (!req)
+		return -1;
+
+	arm_cci_request(ep, req, sizeof(*req_pl) + in->num_vcs,
+			VIRTUAL_SWITCH, GET_VCS_INFO);
+	req_pl = (struct cxlmi_cmd_fmapi_get_vcs_info_req *)req->payload;
+
+	req_pl->start_vppb = in->start_vppb;
+	req_pl->vppb_list_limit = in->vppb_list_limit;
+	req_pl->num_vcs = in->num_vcs;
+	for (int i = 0; i < in->num_vcs; i++)
+		req_pl->vcs_id_list[i] = in->vcs_id_list[i];
+
+	rsp_pl_sz = sizeof(*rsp_pl) +
+		in->num_vcs * (sizeof(struct cxlmi_cmd_fmapi_vcs_info_block) +
+			       in->vppb_list_limit * sizeof(struct cxlmi_cmd_fmapi_vppb_info));
+	rsp_sz = sizeof(*rsp) + rsp_pl_sz;
+	rsp = calloc(1, rsp_sz);
+	if (!rsp)
+		return -1;
+
+	rc = send_cmd_cci(ep, ti, req, req_sz, rsp, rsp_sz, rsp_sz);
+	if (rc)
+		return rc;
+
+	rsp_pl = (struct cxlmi_cmd_fmapi_get_vcs_info_rsp *)rsp->payload;
+
+	/* Copy response - caller must parse variable-length VCS info blocks */
+	memcpy(ret, rsp_pl, rsp_pl_sz);
+
+	return rc;
+}
+
 CXLMI_EXPORT int cxlmi_cmd_fmapi_bind_vppb(struct cxlmi_endpoint *ep,
 			    struct cxlmi_tunnel_info *ti,
 			    struct cxlmi_cmd_fmapi_bind_vppb_req *in)

src/cxlmi/api-types.h
@@ -713,6 +713,35 @@ struct cxlmi_cmd_fmapi_get_domain_validation_sv_rsp {
 	uint8_t secret_value_uuid[0x10];
 } __attribute__((packed));
 
+/* CXL r3.2 Section 7.6.7.2.1: Get Virtual CXL Switch Info (Opcode 5200h) */
+struct cxlmi_cmd_fmapi_get_vcs_info_req {
+	uint8_t start_vppb;
+	uint8_t vppb_list_limit;
+	uint8_t num_vcs;
+	uint8_t vcs_id_list[];
+} __attribute__((packed));
+
+struct cxlmi_cmd_fmapi_vppb_info {
+	uint8_t binding_status;
+	uint8_t bound_port_id;
+	uint8_t bound_ld_id;
+	uint8_t rsv1;
+} __attribute__((packed));
+
+struct cxlmi_cmd_fmapi_vcs_info_block {
+	uint8_t vcs_id;
+	uint8_t vcs_state;
+	uint8_t usp_id;
+	uint8_t num_vppbs;
+	struct cxlmi_cmd_fmapi_vppb_info vppbs[];
+} __attribute__((packed));
+
+struct cxlmi_cmd_fmapi_get_vcs_info_rsp {
+	uint8_t num_vcs;
+	uint8_t rsv1[3];
+	struct cxlmi_cmd_fmapi_vcs_info_block vcs_info_list[];
+} __attribute__((packed));
+
 /* CXL r3.1 Section 7.6.7.2.2: BindvPPB (Opcode 5201h) */
 struct cxlmi_cmd_fmapi_bind_vppb_req {
 	uint8_t vcs_id;

src/cxlmi/private.h
@@ -264,7 +264,8 @@ enum {
 	#define SET_DOMAIN_VALIDATION_SV           0x5
 	#define GET_VCS_DOMAIN_VALIDATION_SV_STATE 0x6
 	#define GET_DOMAIN_VALIDATION_SV           0x7
-	VIRTUAL_SWITCH = 0x52,
+    VIRTUAL_SWITCH = 0x52,
+	#define GET_VCS_INFO  0x0
 	#define BIND_VPPB     0x1
 	#define UNBIND_VPPB   0x2
     MLD_PORT = 0x53,

src/libcxlmi.h
@@ -657,6 +657,11 @@ int cxlmi_cmd_fmapi_get_domain_validation_sv(struct cxlmi_endpoint *ep,
 			struct cxlmi_tunnel_info *ti,
 			struct cxlmi_cmd_fmapi_get_domain_validation_sv_req *in,
 			struct cxlmi_cmd_fmapi_get_domain_validation_sv_rsp *ret);
+
+int cxlmi_cmd_fmapi_get_vcs_info(struct cxlmi_endpoint *ep,
+				 struct cxlmi_tunnel_info *ti,
+				 struct cxlmi_cmd_fmapi_get_vcs_info_req *in,
+				 struct cxlmi_cmd_fmapi_get_vcs_info_rsp *ret);
 int cxlmi_cmd_fmapi_bind_vppb(struct cxlmi_endpoint *ep,
 			struct cxlmi_tunnel_info *ti,
 			struct cxlmi_cmd_fmapi_bind_vppb_req *in);

commit 3827151160bd4f8c1dabc14e8652d3e363413150

commands.c: fixup cxlmi_cmd_fmapi_get_vcs_info
Since the response of command 5200h is variable length, a minimum
payload size is required to prevent fixed_length payload error path being
entered in sanity_check_mctp_rsp(), in the event that max VCS's/VPPBs is
different from the actual number of VCS's/VPPB's returned.

We assume that the minimum returned VCS's is 1, and the minimum number
of returned VPPB's is also 1. It is unclear from the specification
whether it could feasibly be zero returned without returning an error condition.
E.g. if the requested VCS ID is incorrect. We assume in this case the response
should be "Invalid Input", rather than success with zero returned VCS's.

Fixes: 2f14ae0

Signed-off-by: Joshua Lant <joshualant@gmail.com>

src/cxlmi/commands.c
@@ -2420,7 +2420,7 @@ CXLMI_EXPORT int cxlmi_cmd_fmapi_get_vcs_info(struct cxlmi_endpoint *ep,
 	struct cxlmi_cmd_fmapi_get_vcs_info_rsp *rsp_pl;
 	_cleanup_free_ struct cxlmi_cci_msg *req = NULL;
 	_cleanup_free_ struct cxlmi_cci_msg *rsp = NULL;
-	ssize_t req_sz, rsp_sz, rsp_pl_sz;
+	ssize_t req_sz, rsp_sz, rsp_pl_sz, rsp_sz_min;
 	int rc = -1;
 
 	req_sz = sizeof(*req) + sizeof(*req_pl) + in->num_vcs;
@@ -2442,11 +2442,14 @@ CXLMI_EXPORT int cxlmi_cmd_fmapi_get_vcs_info(struct cxlmi_endpoint *ep,
 		in->num_vcs * (sizeof(struct cxlmi_cmd_fmapi_vcs_info_block) +
 			       in->vppb_list_limit * sizeof(struct cxlmi_cmd_fmapi_vppb_info));
 	rsp_sz = sizeof(*rsp) + rsp_pl_sz;
+        rsp_sz_min = sizeof(*rsp) + sizeof(*rsp_pl) +
+            sizeof(struct cxlmi_cmd_fmapi_vcs_info_block) +
+                sizeof(struct cxlmi_cmd_fmapi_vppb_info);
 	rsp = calloc(1, rsp_sz);
 	if (!rsp)
 		return -1;
 
-	rc = send_cmd_cci(ep, ti, req, req_sz, rsp, rsp_sz, rsp_sz);
+	rc = send_cmd_cci(ep, ti, req, req_sz, rsp, rsp_sz, rsp_sz_min);
 	if (rc)
 		return rc;
 